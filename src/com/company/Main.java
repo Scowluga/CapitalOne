d1aa9eaed1358e0ed204759ab7d68c0118644b19
Scowluga <david.scowluga@gmail.com>
2019-01-27 03:06:18 +0000

    Completed. Doing edge cases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    // so you can't recursive this thing
-    // but you can recurse the counting the multi line comments
 
-    // so you can't do this anymore sigh holy shit
+    String s = " asdf ' ' asdf ";
+    String b = ' asdf ';
 
+
+
+
+    // just check NOT cTODO or TODOc or cTODOc
+    // if there's a single one, return 1
+    // TODOs TODOe aTODO TODO*&^%#@!)(P
     private static int countTODOs(String line) {
+        // huh what to do here
         return line.replaceAll("\\W*[tT][oO][dD][oO]\\W*", "").equals(line) ? 0 : 1;
     }
-
-    // Dealing  with Python
-    private static void processPyFile(String fileName) {
-
-    }
-
 
     // Dealing with // /* */ languages
     private static void processFile(String fileName) {
@@ -33,6 +33,9 @@ public class Main {
         boolean isMultiline = false; // if a multi-line comment is going on
 
 
+
+        /*TODO */ /*(TODO:&
+        TODO**/ /*TODOA */ //
 
         /* TODO: 0 */ /*TODO: 1 */ // wtf this counts as 2
         //TODO
@@ -51,21 +54,23 @@ public class Main {
         // \W[tT][oO][dD][oO]\W
 
         // Java Regex for anything that's not a character
+        // YOu also have to check for when the indexOfs are not in a quote
+        // omg or multiline quote nope we're ignoring that case
 
         File file = new File("test/" + fileName);
         try {
             Scanner sc = new Scanner(file);
-            while (sc.hasNextLine()) {
+            gl: while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-
+                nTotalLines++;
 
                 if (line.isEmpty()) {
-                    if (isMultiline) nMultiCommentLines++;
+                    if (isMultiline) {
+                        nCommentLines++;
+                        nMultiCommentLines++;
+                    }
                     continue;
                 }
-
-
-                nTotalLines++;
 
                 if (!isMultiline) {
                     int singleStart = line.indexOf("//");
@@ -75,39 +80,52 @@ public class Main {
                         continue;
                     }
 
-                    if (multiStart == -1 || singleStart < multiStart) {
-                        // single line first
-                        nSingleComments++;
+                    if (multiStart == -1) {
                         nCommentLines++;
+                        nSingleComments++;
                         nTODOs += countTODOs(line.substring(singleStart));
 
-                    } else { // multi line first
+                    } else {
                         isMultiline = true;
+                        nMultiComments++;
                         line = line.substring(multiStart + 2);
                     }
                 }
 
                 if (isMultiline) {
+                    nCommentLines++;
+                    nMultiCommentLines++;
 
+                    while (line.contains("*/")) {
 
-                    nMultiComments++;
-                    if (line.contains("*/")) {
-                        nTODOs += countTODOs(line.substring(0, line.indexOf("*/")));
+                        int multiEnd = line.indexOf("*/");
+                        nTODOs += countTODOs(line.substring(0, multiEnd));
+                        line = line.substring(multiEnd + 2);
                         isMultiline = false;
-                    } else {
-                        nTODOs += countTODOs(line);
+
+                        int singleStart = line.indexOf("//");
+                        int multiStart = line.indexOf("/*");
+
+                        if (singleStart == -1 && multiStart == -1) {
+                            continue gl;
+                        }
+
+                        if (multiStart == -1) {
+                            nSingleComments++;
+                            nTODOs += countTODOs(line.substring(singleStart));
+                            continue gl;
+
+                        } else {
+                            isMultiline = true;
+                            nMultiComments++;
+                            line = line.substring(multiStart + 2);
+                        }
                     }
 
-                    // omg search for another start
+                    nTODOs += countTODOs(line);
 
-
-                } else {
                 }
-
-
-
             }
-
 
             System.out.println("Total # of lines: " + nTotalLines);
             System.out.println("Total # of comment lines: " + nCommentLines);
@@ -119,11 +137,8 @@ public class Main {
             sc.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error opening file: " + e.getMessage());
-            return;
         }
-
     }
-
 
     // Main method
     // Continuously reads in file name as input
@@ -142,9 +157,6 @@ public class Main {
             if (fileName.equals("q"))
                 return;
 
-            if (fileName.length() == 0)
-                continue;
-
             if (fileName.substring(0, 1).equals("."))
                 continue;
 
@@ -154,11 +166,9 @@ public class Main {
             // Calls respective function
             String extension = fileName.substring(fileName.indexOf("."));
 
-            if (extension.equals(".py"))
-                processPyFile(fileName);
-            else if (true) {
-                processFile(fileName);
-            }
+
+            processFile(fileName);
+
 
         }
     }
---
+++ test/stress_test.java
@@ -0,0 +1,12 @@
+/*
+ * Stress test for multi-multi-line comments
+ * Total # of lines: 12
+ * Total # of comment lines: 11
+ * Total # of single line comments: 1
+ * Total # of comment lines within block comments: 11
+ * Total # of block line comments: 4
+ * Total # of TODO's: 3
+ */
+
+/*TODO */ /*(TODO:&
+TODO**/ /*TODOA */ //
