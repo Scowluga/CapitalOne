aecd19198a767cabe3503a93751e1658d9a2d57a
Scowluga <david.scowluga@gmail.com>
2019-01-27 04:54:22 +0000

    Solved second edge case for strings

--- test/stress_test.java
+++ test/stress_test.java
@@ -1,12 +1,12 @@
 /*
- * Stress test for multi-multi-line comments
+ * Stress test
  * Total # of lines: 12
  * Total # of comment lines: 11
  * Total # of single line comments: 1
  * Total # of comment lines within block comments: 11
  * Total # of block line comments: 4
- * Total # of TODO's: 3
+ * Total # of TODOs: 3
  */
 
-/*TODO */ /*(TODO:&
-TODO**/ /*TODOA */ //
+/* TODOnope */ /* TODO:yes */ //
+String y = "//" + "/*" + "*/" + "TODO"; // "TODO"
