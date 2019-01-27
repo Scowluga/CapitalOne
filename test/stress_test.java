1f49c65a77fead947c310906c7b34d746431d498
Scowluga <david.scowluga@gmail.com>
2019-01-27 04:55:07 +0000

    Updating test file

--- test/stress_test.java
+++ test/stress_test.java
@@ -2,10 +2,10 @@
  * Stress test
  * Total # of lines: 12
  * Total # of comment lines: 11
- * Total # of single line comments: 1
- * Total # of comment lines within block comments: 11
- * Total # of block line comments: 4
- * Total # of TODOs: 3
+ * Total # of single line comments: 2
+ * Total # of comment lines within block comments: 10
+ * Total # of block line comments: 3
+ * Total # of TODOs: 2
  */
 
 /* TODOnope */ /* TODO:yes */ //
