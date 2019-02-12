d6cacc4e69f715f1aeace70154f875d9260afe7f
David Lu <david.scowluga@gmail.com>
2019-02-12 15:00:12 -0500

    Lots of really good updates

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,102 @@
-package com.company;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
+    // The fileName is proper, proceed to processFile
+    public static final String MESSAGE_SUCCESS = "";
+    // The input was "q". Quit.
+    public static final String MESSAGE_QUIT = "Goodbye.";
+    // File begins with ".". To be ignored.
+    public static final String MESSAGE_IGNORED = "File type ignored.";
+    // Too many "."s in the fileName
+    public static final String MESSAGE_INVALID = "Enter a valid file name.";
+    // The extension is currently not supported
+    public static final String MESSAGE_UNSUPPORTED = "File extension not supported.";
+
+    // Returns message if extension is incorrect
+    // Otherwise returns empty string meaning start processing
+    public static String checkFile(String fileName) {
+        if (fileName.equals("q"))
+            return MESSAGE_QUIT;
+
+        if (fileName.substring(0, 1).equals("."))
+            return MESSAGE_IGNORED;
+
+        if (fileName.split("\\.").length != 2)
+            return MESSAGE_INVALID;
+
+        Set<String> supportedExtensions = new HashSet<>(Arrays.asList(
+                ".java",
+                ".ts"
+        ));
+
+        // Check for a supported extension
+        String extension = fileName.substring(fileName.indexOf("."));
+        if (!supportedExtensions.contains(extension))
+            return MESSAGE_UNSUPPORTED;
+
+        return MESSAGE_SUCCESS;
+    }
+
     // Counting if the to-do statement exists in a specific string
     // Uses regex to check each possible case
-    private static int countTODOs(String line) {
-        if (line.matches("^[tT][oO][dD][oO]\\W")
-                || line.matches("\\W[tT][oO][dD][oO]$")
+    public static int countTODOs(String line) {
+        if (line.matches("^[tT][oO][dD][oO]\\W.*")
+                || line.matches(".*\\W[tT][oO][dD][oO]$")
                 || line.matches("^[tT][oO][dD][oO]$")
                 || line.matches(".*\\W[tT][oO][dD][oO]\\W.*")) {
-                return 1;
+            return 1;
         }
         return 0;
     }
 
     // Clears strings from a line until either a comment, or new line appears
-    private static String cleanLine(String line) {
+    public static String cleanLine(String line) {
         int singleStart = line.indexOf("//");
         int multiStart = line.indexOf("/*");
         int stringStart = line.indexOf("\"");
+        int charStart = line.indexOf("\'");
 
         if (singleStart == -1) singleStart = Integer.MAX_VALUE;
         if (multiStart == -1) multiStart = Integer.MAX_VALUE;
         if (stringStart == -1) stringStart = Integer.MAX_VALUE;
+        if (charStart == -1) charStart = Integer.MAX_VALUE;
 
         // A string happens before the next comment
-        while (stringStart != Integer.MAX_VALUE
-                && stringStart < Math.min(singleStart, multiStart)
-                ) {
+        while ((
+                stringStart != Integer.MAX_VALUE
+                        && stringStart < Math.min(singleStart, multiStart))
+                || (charStart != Integer.MAX_VALUE
+                        && charStart < Math.min(singleStart, multiStart))) {
 
             // Remove that string with regex
-            line = line.replaceFirst("[\"].*?[\"]", "");
+            if (stringStart < charStart)
+                line = line.replaceFirst("[\"].*?[\"]", "");
+            else
+                line = line.replaceFirst("[\'].*?[\']", "");
 
 
             singleStart = line.indexOf("//");
             multiStart = line.indexOf("/*");
             stringStart = line.indexOf("\"");
+            charStart = line.indexOf("\'");
 
             if (singleStart == -1) singleStart = Integer.MAX_VALUE;
             if (multiStart == -1) multiStart = Integer.MAX_VALUE;
             if (stringStart == -1) stringStart = Integer.MAX_VALUE;
+            if (charStart == -1) charStart = Integer.MAX_VALUE;
         }
 
         return line;
     }
 
     // Processes a specific file name
-    private static void processFile(String fileName) {
+    public static void processFile(String fileName) {
 
         // Output variables
         int nTotalLines = 0; // # non-empty lines
@@ -62,10 +108,11 @@ public class Main {
         boolean isMultiline = false; // if a multi-line comment is going on
 
         // File IO
-        File file = new File("test/" + fileName);
+        File file = new File("input_tests/" + fileName);
         try {
             Scanner sc = new Scanner(file);
-            gl: while (sc.hasNextLine()) {
+            gl:
+            while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 nTotalLines++;
 
@@ -135,9 +182,7 @@ public class Main {
                             line = line.substring(multiStart + 2);
                         }
                     }
-
                     nTODOs += countTODOs(line);
-
                 }
             }
 
@@ -161,37 +206,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
 
-            // Input with Scanner for simplicity
+            // Setting up input
             System.out.println("--------------------------------");
             System.out.println("Enter a file name or 'q' to quit");
             System.out.print("> ");
 
+            // Reading input
             String fileName = scanner.nextLine().trim();
+            String message = checkFile(fileName);
 
-            // Processing file name
-            if (fileName.equals("q")) {
-                System.out.println("Goodbye.");
-                return;
+            switch (message) {
+                case MESSAGE_QUIT:
+                    System.out.println(message);
+                    return;
+                case MESSAGE_SUCCESS:
+                    processFile(fileName);
+                    break;
+                default:
+                    System.out.println(message);
+                    break;
             }
-
-            if (fileName.substring(0, 1).equals(".")) {
-                System.out.println("File type ignored.");
-                continue;
-            }
-
-            if (fileName.split("\\.").length != 2) {
-                System.out.println("Enter a valid file name.");
-                continue;
-            }
-
-            // Check for a supported extension
-            String extension = fileName.substring(fileName.indexOf("."));
-            if (!(extension.equals(".java") || extension.equals(".ts"))) {
-                System.out.println("File extension not supported.");
-                continue;
-            }
-
-            processFile(fileName);
         }
     }
 }
